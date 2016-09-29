f9c15fea2533b72b4380150a8c142bfc52b2414b
Ulric Wilfred <shaman.sir@gmail.com>
2016-09-29 11:35:41 +0200

    the generic patter for a Preview code is ready

--- lib/compilers/elm-compiler/temp/PreviewDraft.elm
+++ lib/compilers/elm-compiler/temp/PreviewDraft.elm
@@ -1,52 +1,64 @@
 module Main exposing (..)
 
-import String
-import Html.App as Html
 import Html exposing (..)
 import UserCode exposing (..)
-import Html.App exposing (beginnerProgram, program)
 
 import Ast exposing (parseExpression, parseStatement, parse)
+import Ast.BinOp exposing (operators)
 import Ast.Expression exposing (Expression(..))
-import Ast.Statement exposing (ExportSet(..), Type(..), Statement(..))
 
 add2 : Int -> Int -> Int
 add2 a b = a + b
+
+type ParseError = UnknownExpression String
 
 charComponent : Char -> Html Msg
 charComponent ch =
     span [] [ text (toString ch) ]
 
-renderExpression : String -> Expression -> Html Msg
-renderExpression expressionString expressionType  =
-    case expressionType of
-        Character ch -> (charComponent ch)
-        _ -> (div [] [ text expressionString ])
-    --   | String String
-    --   | Integer Int
-    --   | Float Float
-    --   | Variable (List Name)
-    --   | Range Expression Expression
-    --   | List (List Expression)
-    --   | Access Expression (List Name)
-    --   | Record (List (Name, Expression))
-    --   | RecordUpdate Name (List (Name, Expression))
-    --   | If Expression Expression Expression
-    --   | Let (List (Name, Expression)) Expression
-    --   | Case Expression (List (Expression, Expression))
-    --   | Lambda (List Name) Expression
-    --   | Application Expression Expression
-    --   | BinOp Expression Expression Expression
+getExpressionResult : String -> (String, Result ParseError Expression)
+getExpressionResult expressionString =
+    case parseExpression operators expressionString of
+        (Ok r, _) -> (expressionString, Ok r)
+        _ -> (expressionString, Err (UnknownExpression expressionString))
 
-renderAllExpressions : List String -> Html Msg
-renderAllExpressions =
-    List.map (\expressionString -> (renderExpression expressionString parseExpression))
+renderExpression : (String, Result ParseError Expression) -> Html Msg
+renderExpression (expressionString, expressionResult)  =
+    case expressionResult of
+        Ok expressionType ->
+            case expressionType of
+                Character ch -> (charComponent ch)
+                _ -> (div [] [ text expressionString ])
+                --   | String String
+                --   | Integer Int
+                --   | Float Float
+                --   | Variable (List Name)
+                --   | Range Expression Expression
+                --   | List (List Expression)
+                --   | Access Expression (List Name)
+                --   | Record (List (Name, Expression))
+                --   | RecordUpdate Name (List (Name, Expression))
+                --   | If Expression Expression Expression
+                --   | Let (List (Name, Expression)) Expression
+                --   | Case Expression (List (Expression, Expression))
+                --   | Lambda (List Name) Expression
+                --   | Application Expression Expression
+                --   | BinOp Expression Expression Expression
+        Err error ->
+            case error of
+                UnknownExpression message -> span [] [ text message ]
 
+renderAllExpressions : List String -> List (Html Msg)
+renderAllExpressions expressionStrings =
+    List.map renderExpression (List.map getExpressionResult expressionStrings)
+
+expressions : List String
 expressions = [
       toString (add 1)
     , toString (add2 3)
     , toString (add2 3 5)
     ]
 
+main : Html Msg
 main =
-    renderAllExpressions expressions
+    div [] (renderAllExpressions expressions)
