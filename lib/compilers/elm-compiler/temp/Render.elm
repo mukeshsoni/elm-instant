96011348a24d2a88c638dc42bba4c4774bc24fa9
Ulric Wilfred <shaman.sir@gmail.com>
2016-10-02 19:13:34 +0200

    improve render flow and [almost] properly unpack lists

--- lib/compilers/elm-compiler/temp/Render.elm
+++ lib/compilers/elm-compiler/temp/Render.elm
@@ -45,62 +45,71 @@ listComponent : List Expression -> Html a
 listComponent expressions =
     ul [ class "list" ]
        (List.map
-        (\expression -> li [] [ renderExpressionDirectly expression ])
+        (\expression -> li [] (unpackListCell expression))
         expressions)
 
 lambdaComponent : List String -> Expression -> Html a
 lambdaComponent names expression =
     dl [ class "lambda" ]
        [ dt [] [ text (String.join "," (List.map toString names)) ]
-       , dd [] [ renderExpressionDirectly expression ]
+       , dd [] [ renderExpression expression ]
        ]
 
-binopComponent : Expression -> Expression -> Expression -> Html a
-binopComponent operator expressionL expressionR =
-    div [ class "operator" ]
-        [ span [ class "operator-is" ] [ renderExpressionDirectly operator ]
-        , div [ class "left-expression" ]  [ renderExpressionDirectly expressionL ]
-        , div [ class "right-expression" ]  [ renderExpressionDirectly expressionR ]
-        ]
+-- binopComponent : Expression -> Expression -> Expression -> Html a
+-- binopComponent operator expressionL expressionR =
+--     div [ class "operator" ]
+--         [ span [ class "operator-is" ] [ renderExpressionDirectly operator ]
+--         , div [ class "left-expression" ]  [ renderExpressionDirectly expressionL ]
+--         , div [ class "right-expression" ]  [ renderExpressionDirectly expressionR ]
+--         ]
 
 notIdentifiedExpressionComponent : String -> Html a
 notIdentifiedExpressionComponent expressionString =
     div [ class "not-identitied" ] [ text expressionString ]
 
-getExpressionResult : String -> (String, Result ParseError Expression)
+emptyLinesComponent : Int -> Html a
+emptyLinesComponent howMuch =
+    div [ style [ ( "white-space", "pre") ] ]
+        [ text (String.repeat howMuch "\n") ]
+
+unpackListCell : Expression -> List (Html a)
+unpackListCell cell =
+    case cell of
+        BinOp op l r -> List.append (unpackListCell l) (unpackListCell r)
+        v -> [ renderExpression v ]
+
+getExpressionResult : String -> Result ParseError Expression
 getExpressionResult expressionString =
     case parseExpression operators expressionString of
-        (Ok r, _) -> (expressionString, Ok r)
-        _ -> (expressionString, Err (UnknownExpression expressionString))
+        (Ok r, _) -> Ok r
+        _ -> Err (UnknownExpression expressionString)
 
-renderExpressionDirectly : Expression -> Html a
-renderExpressionDirectly expression =
-    renderExpression ("?",  Ok expression)
+renderExpression : Expression -> Html a
+renderExpression expression =
+    case expression of
+        Character ch -> (charComponent ch)
+        String str -> (stringComponent str)
+        Integer int -> (integerComponent int)
+        Float float -> (floatComponent float)
+        Variable names -> (variableComponent names)
+        --   | Range Expression Expression
+        List expressions -> (listComponent expressions)
+        --   | Access Expression (List Name)
+        --   | Record (List (Name, Expression))
+        --   | RecordUpdate Name (List (Name, Expression))
+        --   | If Expression Expression Expression
+        --   | Let (List (Name, Expression)) Expression
+        --   | Case Expression (List (Expression, Expression))
+        Lambda names expression -> (lambdaComponent names expression)
+        --   | Application Expression Expression
+        -- BinOp operator expressionL expressionR ->
+            -- (binopComponent operator expressionL expressionR)
+        _ -> (notIdentifiedExpressionComponent (toString expression))
 
-renderExpression : (String, Result ParseError Expression) -> Html a
-renderExpression (expressionString, expressionResult)  =
-    case expressionResult of
-        Ok expressionType ->
-            case expressionType of
-                Character ch -> (charComponent ch)
-                String str -> (stringComponent str)
-                Integer int -> (integerComponent int)
-                Float float -> (floatComponent float)
-                Variable names -> (variableComponent names)
-                --   | Range Expression Expression
-                List expressions -> (listComponent expressions)
-                --   | Access Expression (List Name)
-                --   | Record (List (Name, Expression))
-                --   | RecordUpdate Name (List (Name, Expression))
-                --   | If Expression Expression Expression
-                --   | Let (List (Name, Expression)) Expression
-                --   | Case Expression (List (Expression, Expression))
-                Lambda names expression -> (lambdaComponent names expression)
-                --   | Application Expression Expression
-                BinOp operator expressionL expressionR ->
-                    (binopComponent operator expressionL expressionR)
-                _ -> (notIdentifiedExpressionComponent expressionString)
-
+renderExpressionFromString : String -> Html a
+renderExpressionFromString expressionString  =
+    case getExpressionResult expressionString of
+        Ok expressionType -> renderExpression expressionType
         Err error ->
             case error of
                 UnknownExpression message -> (errorComponent message)
@@ -109,10 +118,9 @@ renderPreviewItem : PreviewItem -> Html a
 renderPreviewItem previewItem =
     case previewItem of
         UserExpression expressionString ->
-            renderExpression (getExpressionResult expressionString)
+            renderExpressionFromString expressionString
         EmptyLines howMuch ->
-            div [ style [ ( "white-space", "pre") ] ]
-                [ text (String.repeat howMuch "\n") ]
+            emptyLinesComponent howMuch
 
 renderAll : List PreviewItem -> List (Html a)
 renderAll previewItems =
