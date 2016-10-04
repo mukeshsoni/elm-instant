2995de41ba5176475571c4de5c2b0ce8689623ee
Mukesh Soni <mukeshsoni@gmail.com>
2016-10-04 22:50:58 +0530

    added an npm script to take care of installing pre packaged elm dependencies

---
+++ elm-scripts.js
@@ -0,0 +1,13 @@
+var Promise = require('bluebird')
+const writeFile = Promise.promisify(require('fs').writeFile)
+var path = require('path')
+var exec = Promise.promisify(require('child_process').exec)
+
+process.chdir('lib/compilers/elm-compiler/temp')
+var elmPackageJson = require(path.join(process.cwd(), 'elm-package-template.js'))
+
+exec('rm -rf elm-stuff elm-package.json')
+.then(() => writeFile('elm-package.json', JSON.stringify(elmPackageJson)))
+.then(() => exec('elm-package install -y'))
+.then(console.log)
+.catch(console.error)
